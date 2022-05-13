5e296d646606c4ed04452bf8fbda21704aeec192
Omar <omarmeciry21@gmail.com>
2022-05-13 20:55:46 +0200

    first commi

---
+++ main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+/*    float gpa;
+    cout<<"Enter your GPA: ";
+    cin>>gpa;
+
+    if(gpa >=3.6){
+        cout<<'A';
+    }
+    else if(gpa>=3){
+        cout<<'B';
+    }
+    else if(gpa>=2.5){
+        cout<<'C';
+    }
+    else{
+        cout<<"D";
+    }*/
+/*
+    int categoryId = 5;
+
+    if(categoryId==1){
+        cout<<"Mobiles";
+    }
+    else if(categoryId ==2){
+        cout<<"Laptops";
+    }
+    else if(categoryId ==3){
+        cout<<"Computers";
+    }
+    else if(categoryId ==4){
+        cout<<"Headphones";
+    }
+    else if(categoryId ==5){
+        cout<<"Power chargers";
+    }
+*/
+  /*  int categoryId = 2;
+    switch(categoryId){
+        case 1:
+            cout<<"Mobiles"; break;
+        case 2:
+            cout<<"Laptops\n";
+            cout<<"Thanks for using our shop";
+            break;
+        case 3:
+            cout<<"Computers"; break;
+        case 4:
+            cout<<"Headphones"; break;
+        case 5:
+            cout<<"Power chargers"; break;
+    }*/
+
+
+    //Loops
+    /*int num;
+    cout<<"Enter number of 'Hello's: ";
+    cin>>num;
+    for(int i=1;i<=num;i++){
+        cout<<"Hello World!"<<endl;
+    }*/
+
+    /*int num;
+    cin>>num;
+
+    int sum=0;
+    for(int i=1;i<=num;i++){
+        sum += i;
+    }
+
+    cout<<"Sum = "<<sum;*/
+
+    /*int i=1, num,sum=0;
+    cin>>num;
+
+    while(i<=num){
+        sum+=i;
+        i++;
+    }
+
+    cout<<"Sum ="<<sum;*/
+
+
+    /*int num ; cin>>num;
+
+    for(int i =num;i>=1;i--){
+        if(num%i==0)
+        {
+            cout<<i<<" ";
+        }
+    }*/
+
+    /*int num ; cin>>num;
+    int i =num;
+    while(i>0){
+        if(num%i==0) cout<<i<<" ";
+        i--;
+    }*/
+
+    int n1 = 10;
+    int n2 =3;
+    float number = (float)n1/n2;
+    cout<< number;
+    return 0;
+}
